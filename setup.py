7ad1f9021d7144d33361c3246ffa9d31818809f7
JacobTh98 <jacob.thoenes@uni-rostock.de>
2025-10-15 15:20:10 +0200

    v and readme

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="sciopy",
-    version="0.8.2",
+    version="0.8.9",
     packages=find_packages(),
     author="Jacob Peter Thönes",
     author_email="jacob.thoenes@uni-rostock.de",
