4d6b27b9e03b89291c3dca94c884cd4269633270
JacobTh98 <jacob.thoenes@uni-rostock.de>
2026-04-29 14:02:16 +0200

    v0.9.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="sciopy",
-    version="0.8.2.3",
+    version="0.9.0",
     packages=find_packages(),
     author="Jacob P. Thönes",
     author_email="jacob.thoenes@uni-rostock.de",
